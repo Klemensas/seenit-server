2d251cfe78dc95c8b3c8e1ceef7cd61e6cb265a9
Klemensas <hello@klemensas.net>
2019-10-17 19:26:12 +0300

    chore: improve server path log

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { Server } from './server';
 import { config } from './config';
 
 Server.initializeApp().then(() => {
-  console.log(('App is running at http://localhost:%d in %s mode'), config.port, config.env);
+  console.log(('App is running at %s://%s:%d in %s mode'), config.tls.certPath ? 'https' : 'http', config.ip, config.port, config.env);
 });
